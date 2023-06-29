92783a14a92b1761c3aff995d769bbda888c48bb
Feruz <feruz.muradoff.93@mail.ru>
2023-06-29 10:14:49 +0500

    done all the tasks

---
+++ Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3 */
+
+int[] Array = new int[5];
+for (int i = 0; i < 5; i++)
+{
+    Array[i] = new Random().Next(-100, 101);
+}
+Console.WriteLine(string.Join(",", Array));
+int count = 0;
+foreach (int el in Array)
+{
+    count += el>0 ? 1:0;
+}
+Console.WriteLine(count);
---
+++ Task3_additional/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Доп.Задача Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1. 
+(Решить можно, если хотите потренироваться)
+(Следующее число складывается из двух предыдущих)
+
+Если N = 5 -> 0 1 1 2 3
+
+Если N = 3 -> 0 1 1
+
+Если N = 7 -> 0 1 1 2 3 5 8 */
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+if (num == 0 || num == 1)
+{
+    Console.WriteLine("Не допустимое число");
+}
+else
+{
+    int[] Array = new int[num];
+    Array[0] = 0;
+    Array[1] = 1;
+    for (int i = 2; i < num; i++)
+    {
+        Array[i] = Array[i - 2] + Array[i - 1];
+    }
+    Console.WriteLine(string.Join(", ", Array));
+}
